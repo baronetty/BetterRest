5a946a24ba3f9dcdfb72e2a56bceec2eceb1439e
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-09 16:24:34 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Leo  on 09.11.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
